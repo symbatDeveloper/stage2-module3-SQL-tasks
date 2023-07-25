ea46b214c85b87fabdc111c4a7360ad1e1fc90de
symbatDeveloper <symbatzinatova.k@gmail.com>
2023-07-26 00:35:36 +0600

    fixed

--- lesson3/src/main/resources/SOLUTION.sql
+++ lesson3/src/main/resources/SOLUTION.sql
@@ -2,10 +2,6 @@
  ALTER TABLE Mark ALTER colum mark ADD CHECK (mark >= 1 AND mark <= 10);
  ALTER TABLE Mark ALTER colum student_id SET NOT NULL;
  ALTER TABLE Mark ALTER colum subject_id SET NOT NULL;
- 4) Change table ‘PaymentType’
-    ‘name’ should be a unique value
- 5) Change table ‘Payment’
-    ‘type_id’, ‘amount’ and ‘date’ cannot be null
 
  ALTER TABLE Subject ALTER colum grade ADD CHECK (mark >= 1 AND mark <= 5);
  ALTER TABLE PaymentType ALTER colum UNIQUE(name);
--- lesson8/src/main/resources/SOLUTION.sql
+++ lesson8/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
-select * from student ORDER BY birthday ASC LIMIT 1;
+select max(birthday) from student;
 SELECT min(payment_date) * FROM payment;
 SELECT AVG(mark) FROM Mark  join  subject on (subject.id = mark.subject_id) where subject.name='Math';
 select min(amount) from payment join paymenttype on (payment.type_id = paymenttype.id) where paymenttype.name = 'WEEKLY';
